067a3c971c34c6b1dbf1590dcf91f7676473bacf
virneprit2 <rituraj.nepal+2@virnex.fi>
2026-04-29 17:00:32 +0000

    addition of tess

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -45,6 +45,15 @@ def test_get_nth_fibonacci_zero():
     # Assert
     assert result == 0
 
+def test_area_of_circle_negative_radius():
+   """Test with a negative radius to raise ValueError."""
+   # Arrange
+   radius = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      area_of_circle(radius)
+
 
 def test_get_nth_fibonacci_one():
     """Test with n=1."""
@@ -68,3 +77,12 @@ def test_get_nth_fibonacci_ten():
 
      # Assert
      assert result == 55
+
+def test_get_nth_fibonacci_negative():
+   """Test with a negative number to raise ValueError."""
+   # Arrange
+   n = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
